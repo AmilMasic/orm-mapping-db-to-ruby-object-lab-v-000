76121be50a1bee84d2939b39d4b1de07aa9e72e7
Amil Masic <amil.masic@gmail.com>
2019-03-31 20:04:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,16 +28,16 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
   end
-  def self.all_students_in_grade_9(name)
+  def self.all_students_in_grade_9(grade)
     sql = <<-SQL
       SELECT *
       FROM students
       WHERE grade = 9
       SQL
 
-      DB[:conn].execute(sql, name).map do |row|
+      DB[:conn].execute(sql, grade).map do |row|
         self.new_from_db(row)
-      end
+      end.first
     # find the student in the database given a name
     # return a new instance of the Student class
   end
